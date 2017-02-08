9ffdfbe857581c678242a8ec838be96863970a63
William Barela <william.barela.dev@gmail.com>
2017-02-08 23:02:59 +0900

    update

--- js/app.js
+++ js/app.js
@@ -1,7 +1,10 @@
 (function(){
   'use strict';
 
-  angular.module('lister', []).controller('menuServer', menuServer);
+  var app = angular.module('lister', [])
+
+  app.controller('menuServer', menuServer);
+  app.controller('formController', formController);
 
   menuServer.$inject = ['$scope'];
   function menuServer($scope){
@@ -10,6 +13,19 @@
       console.log($scope.myData[0].object.name);
     }
   }
+
+  formController.$inject = ['$scope'];
+  function formController($scope){
+    $scope.units = JSON.parse(physics_units);
+    $scope.makeList = function(){
+      console.log($scope.units);
+    }
+  }
+
+
+
+
+
 
   var data = '[{"choice": "blue or red", \
                 "blue": "forget it all", \
@@ -21,29 +37,10 @@
                   {"property": "energy", "object": {"name": "bowling ball", "mass": 7.26, "acceleration":9.8, "init-distance":100}, "units": {"mass": "kg", "distance": "m"}, "colors":{"red": "open your mind to the matrix"}}, \
                   {"property": "energy", "object": {"name": "bowling ball", "mass": 7.26, "acceleration":9.8, "init-distance":100}, "units": {"mass": "kg", "distance": "m"}, "colors":{"red": "open your mind to the matrix"}} \
                 ]';
-
-/*
-  var physics = '[{"property": "energy", \
-                  "object": {"name": "bowling ball", "mass": 7.26, "acceleration":9.8, "init-distance":100}, \
-                  "units": {"mass": "kg", "distance": "m"}, \
-                  {"red": "open your mind to the matrix"}]';
-*/
-
-
-/*
-  var physics = '[{"property": "energy", "object": {"name": "bowling ball", "mass": 7.26, "acceleration":9.8, "init-distance":100}}, \
-                {"units": {"mass": "kg", "distance": "m"}},  \
-                {"red": "open your mind to the matrix"}]';
-*/
-
-/*
-  var physics = '[{"property": "energy", "object": {"name": "bowling ball", "mass": "7.26", "acceleration":"9.8", "init-distance":"100"}}, \
-                "units": {"mass": "kg","distance": "m"},  \
-                "headline": "How much energy (work) does it take to lift a bowling ball 100 meters?", \
-                {"red": "open your mind to the matrix"}]';
-*/
-
-
-
+  var physics_units = '[ \
+                        {"name": "kg"}, \
+                        {"name": "m"}, \
+                        {"name": "m/s^2"} \
+                        ]'
 
 })();
